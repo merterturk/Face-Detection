26dfce63150c8db586b95495e7467f065b668b14
Mert <meerterturk@gmail.com>
2021-05-23 18:03:27 +0300

    Add project files.

---
+++ FaceDetection/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace FaceDetection
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pctFace = new System.Windows.Forms.PictureBox();
+            this.btnFaceDetection = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pctFace)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pctFace
+            // 
+            this.pctFace.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pctFace.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pctFace.Location = new System.Drawing.Point(0, 0);
+            this.pctFace.Name = "pctFace";
+            this.pctFace.Size = new System.Drawing.Size(799, 391);
+            this.pctFace.TabIndex = 0;
+            this.pctFace.TabStop = false;
+            // 
+            // btnFaceDetection
+            // 
+            this.btnFaceDetection.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnFaceDetection.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnFaceDetection.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnFaceDetection.Location = new System.Drawing.Point(237, 409);
+            this.btnFaceDetection.Name = "btnFaceDetection";
+            this.btnFaceDetection.Size = new System.Drawing.Size(287, 29);
+            this.btnFaceDetection.TabIndex = 1;
+            this.btnFaceDetection.Text = "Yüzleri Bul";
+            this.btnFaceDetection.UseVisualStyleBackColor = false;
+            this.btnFaceDetection.Click += new System.EventHandler(this.btnFaceDetection_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnFaceDetection);
+            this.Controls.Add(this.pctFace);
+            this.Name = "Form1";
+            this.Text = "Face Detection Application";
+            ((System.ComponentModel.ISupportInitialize)(this.pctFace)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pctFace;
+        private System.Windows.Forms.Button btnFaceDetection;
+    }
+}
+
---
+++ FaceDetection/Form1.cs
@@ -0,0 +1,63 @@
+﻿using Emgu.CV;
+using Emgu.CV.Structure;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FaceDetection
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Bu sınıftan türetilen nesne sayesinde görsel içindeki yüzleri tanıma, belirleme işlemini yapabilirim.
+          CascadeClassifier cascadeClassifier = new CascadeClassifier("haarcascade_frontalface_alt_tree.xml");
+        private void btnFaceDetection_Click(object sender, EventArgs e)
+        {
+            //Butona bastığımda open file dialog penceresinin açılmasını sağlıyorum. Burada çoklu seçime izin vermedim. Filtreleme kısmında da JPEG ve jpg dosyalarını filtreledim.
+            using (OpenFileDialog ofd = new OpenFileDialog() { Multiselect = false, Filter = "*.JPEG | *.jpg" })
+            {
+
+                //Eğer bir dosya seçilmiş ise bu dosyayı pictureboxa ekliyorum. Pictureboxa eklediğim görseli Bitmap sınfından türettiğim nesnenin içine atıyorum.
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    pctFace.Image = Image.FromFile(ofd.FileName);
+                    Bitmap gorsel = new Bitmap(pctFace.Image);
+
+                    //Görselin dönüşüm işlemini (byte) yapıyorum.
+                    Image<Bgr, byte> goruntu = new Image<Bgr, byte>(gorsel);
+
+                    //Dikdörtgen sınıfından neste türetiyorum
+                    Rectangle[] cerceveler = cascadeClassifier.DetectMultiScale(goruntu, 1.4, 0);
+
+                    foreach (Rectangle cerceve in cerceveler)
+                    {
+                        //Didörtgen çizdiriyorum.
+                        using (Graphics grafik = Graphics.FromImage(gorsel))
+                        {
+                            using (Pen kalem = new Pen(Color.DarkRed, 1))
+                            {
+                                grafik.DrawRectangle(kalem, cerceve);
+                            }
+                        }
+                    }
+
+                    pctFace.Image = gorsel;
+
+                }
+            }
+        }
+
+       
+    }
+}
+    
